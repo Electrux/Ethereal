a926cb647ab3b3160fc366118e2768a689ee594a
Electrux <ElectruxRedsworth@gmail.com>
2019-09-01 15:36:30 +0530

    created insert and delete member functions for mod_map similar to the subscript one

--- stdlib/map.cpp
+++ stdlib/map.cpp
@@ -12,7 +12,7 @@
 var_base_t * insert( vm_state_t & vm, func_call_data_t & fcd )
 {
 	std::unordered_map< std::string, var_base_t * > & map = AS_MAP( fcd.args[ 0 ] )->get();
-	std::string key = fcd.args[ 1 ]->to_str();
+	std::string & key = AS_STR( fcd.args[ 1 ] )->get();
 	if( map.find( key ) != map.end() ) {
 		VAR_DREF( map[ key ] );
 	}
@@ -23,7 +23,7 @@ var_base_t * insert( vm_state_t & vm, func_call_data_t & fcd )
 var_base_t * _delete( vm_state_t & vm, func_call_data_t & fcd )
 {
 	std::unordered_map< std::string, var_base_t * > & map = AS_MAP( fcd.args[ 0 ] )->get();
-	std::string key = fcd.args[ 1 ]->to_str();
+	std::string & key = AS_STR( fcd.args[ 1 ] )->get();
 	if( map.find( key ) != map.end() ) {
 		VAR_DREF( map[ key ] );
 		map.erase( key );
@@ -35,7 +35,7 @@ var_base_t * _delete( vm_state_t & vm, func_call_data_t & fcd )
 var_base_t * get( vm_state_t & vm, func_call_data_t & fcd )
 {
 	std::unordered_map< std::string, var_base_t * > & map = AS_MAP( fcd.args[ 0 ] )->get();
-	std::string key = fcd.args[ 1 ]->to_str();
+	std::string & key = AS_STR( fcd.args[ 1 ] )->get();
 	if( map.find( key ) != map.end() ) {
 		return map[ key ];
 	}
@@ -64,8 +64,8 @@ REGISTER_MODULE( map )
 {
 	functions_t & mapfns = vm.typefuncs[ "map" ];
 
-	mapfns.add( { "insert", 2, 2, { "str", "_any_" }, FnType::MODULE, { .modfn = insert }, false } );
-	mapfns.add( { "delete", 1, 1, { "str" }, FnType::MODULE, { .modfn = _delete }, false } );
+	mapfns.add( { "_insert", 2, 2, { "str", "_any_" }, FnType::MODULE, { .modfn = insert }, false } );
+	mapfns.add( { "_delete", 1, 1, { "str" }, FnType::MODULE, { .modfn = _delete }, false } );
 	mapfns.add( { "get", 1, 1, { "str" }, FnType::MODULE, { .modfn = get }, false } );
 	mapfns.add( { "len", 0, 0, {}, FnType::MODULE, { .modfn = len }, true } );
 	mapfns.add( { "clear", 0, 0, {}, FnType::MODULE, { .modfn = clear }, false } );

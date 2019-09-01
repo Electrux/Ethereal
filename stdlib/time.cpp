e4786b459f4f8f0e534e3c7573066bf3e56c4ed5
Electrux <ElectruxRedsworth@gmail.com>
2019-09-01 10:16:22 +0530

    renamed var_time_t -> var_time_point_t

--- stdlib/time.cpp
+++ stdlib/time.cpp
@@ -11,14 +11,18 @@
 
 #include "../src/VM/Core.hpp"
 
-typedef std::chrono::high_resolution_clock::time_point _time_t;
+typedef std::chrono::high_resolution_clock::time_point time_point_t;
 
-class var_time_t : public var_base_t
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////// Classes ///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class var_time_point_t : public var_base_t
 {
-	_time_t m_time;
+	time_point_t m_time;
 public:
-	var_time_t( const _time_t & time, const int parse_ctr = 0 );
-	~var_time_t();
+	var_time_point_t( const time_point_t & time, const int parse_ctr = 0 );
+	~var_time_point_t();
 
 	std::string type_str() const;
 	std::string to_str() const;
@@ -26,71 +30,75 @@ public:
 	bool to_bool() const;
 	var_base_t * copy( const int parse_ctr );
 	void assn( var_base_t * b );
-	_time_t & get();
+	time_point_t & get();
 };
-#define AS_TIME( x ) static_cast< var_time_t * >( x )
+#define AS_TIME_POINT( x ) static_cast< var_time_point_t * >( x )
 
-var_time_t::var_time_t( const _time_t & time, const int parse_ctr )
+var_time_point_t::var_time_point_t( const time_point_t & time, const int parse_ctr )
 	: var_base_t( VT_CUSTOM, true, parse_ctr ), m_time( time ) {}
-var_time_t::~var_time_t() {}
+var_time_point_t::~var_time_point_t() {}
 
-std::string var_time_t::type_str() const { return "time_t"; }
-std::string var_time_t::to_str() const
+std::string var_time_point_t::type_str() const { return "time_point_t"; }
+std::string var_time_point_t::to_str() const
 {
 	return std::to_string( std::chrono::duration_cast< std::chrono::seconds >( m_time.time_since_epoch() ).count() );
 }
-mpz_class var_time_t::to_int() const { return ( size_t )std::chrono::duration_cast< std::chrono::seconds >( m_time.time_since_epoch() ).count(); }
-bool var_time_t::to_bool() const { return true; }
-var_base_t * var_time_t::copy( const int parse_ctr )
+mpz_class var_time_point_t::to_int() const { return ( size_t )std::chrono::duration_cast< std::chrono::seconds >( m_time.time_since_epoch() ).count(); }
+bool var_time_point_t::to_bool() const { return true; }
+var_base_t * var_time_point_t::copy( const int parse_ctr )
 {
-	return new var_time_t( m_time, parse_ctr );
+	return new var_time_point_t( m_time, parse_ctr );
 }
-void var_time_t::assn( var_base_t * b )
+void var_time_point_t::assn( var_base_t * b )
 {
-	m_time = AS_TIME( b )->get();
+	m_time = AS_TIME_POINT( b )->get();
 }
-_time_t & var_time_t::get() { return m_time; }
+time_point_t & var_time_point_t::get() { return m_time; }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////// Functions //////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var_base_t * time_now( vm_state_t & vm, func_call_data_t & fcd )
 {
-	return new var_time_t( std::chrono::high_resolution_clock::now() );
+	return new var_time_point_t( std::chrono::high_resolution_clock::now() );
 }
 
 var_base_t * time_to_hrs( vm_state_t & vm, func_call_data_t & fcd )
 {
-	_time_t & time = AS_TIME( fcd.args[ 0 ] )->get();
+	time_point_t & time = AS_TIME_POINT( fcd.args[ 0 ] )->get();
 	return new var_int_t( ( size_t )std::chrono::duration_cast< std::chrono::hours >( time.time_since_epoch() ).count() );
 }
 
 var_base_t * time_to_mins( vm_state_t & vm, func_call_data_t & fcd )
 {
-	_time_t & time = AS_TIME( fcd.args[ 0 ] )->get();
+	time_point_t & time = AS_TIME_POINT( fcd.args[ 0 ] )->get();
 	return new var_int_t( ( size_t )std::chrono::duration_cast< std::chrono::minutes >( time.time_since_epoch() ).count() );
 }
 
 var_base_t * time_to_secs( vm_state_t & vm, func_call_data_t & fcd )
 {
-	_time_t & time = AS_TIME( fcd.args[ 0 ] )->get();
+	time_point_t & time = AS_TIME_POINT( fcd.args[ 0 ] )->get();
 	return new var_int_t( ( size_t )std::chrono::duration_cast< std::chrono::seconds >( time.time_since_epoch() ).count() );
 }
 
 var_base_t * time_to_msecs( vm_state_t & vm, func_call_data_t & fcd )
 {
-	_time_t & time = AS_TIME( fcd.args[ 0 ] )->get();
+	time_point_t & time = AS_TIME_POINT( fcd.args[ 0 ] )->get();
 	return new var_int_t( ( size_t )std::chrono::duration_cast< std::chrono::milliseconds >( time.time_since_epoch() ).count() );
 }
 
 var_base_t * time_to_usecs( vm_state_t & vm, func_call_data_t & fcd )
 {
-	_time_t & time = AS_TIME( fcd.args[ 0 ] )->get();
+	time_point_t & time = AS_TIME_POINT( fcd.args[ 0 ] )->get();
 	return new var_int_t( ( size_t )std::chrono::duration_cast< std::chrono::microseconds >( time.time_since_epoch() ).count() );
 }
 
 var_base_t * time_diff( vm_state_t & vm, func_call_data_t & fcd )
 {
-	_time_t & time1 = AS_TIME( fcd.args[ 1 ] )->get();
-	_time_t & time2 = AS_TIME( fcd.args[ 0 ] )->get();
-	return new var_time_t( _time_t( time1 - time2 ) );
+	time_point_t & time1 = AS_TIME_POINT( fcd.args[ 1 ] )->get();
+	time_point_t & time2 = AS_TIME_POINT( fcd.args[ 0 ] )->get();
+	return new var_time_point_t( time_point_t( time1 - time2 ) );
 }
 
 REGISTER_MODULE( time )
@@ -98,12 +106,12 @@ REGISTER_MODULE( time )
 	functions_t & timemodfns = vm.typefuncs[ "_time_t" ];
 	timemodfns.add( { "now", 0,  0, {}, FnType::MODULE, { .modfn = time_now }, true } );
 
-	functions_t & timefns = vm.typefuncs[ "time_t" ];
+	functions_t & timefns = vm.typefuncs[ "time_point_t" ];
 	timefns.add( { "hours", 0,  0, {}, FnType::MODULE, { .modfn = time_to_hrs }, true } );
 	timefns.add( { "mins",  0,  0, {}, FnType::MODULE, { .modfn = time_to_mins }, true } );
 	timefns.add( { "secs",  0,  0, {}, FnType::MODULE, { .modfn = time_to_secs }, true } );
 	timefns.add( { "msecs", 0,  0, {}, FnType::MODULE, { .modfn = time_to_msecs }, true } );
 	timefns.add( { "usecs", 0,  0, {}, FnType::MODULE, { .modfn = time_to_usecs }, true } );
 
-	vm.funcs.add( { "-", 2,  2, { "time_t", "time_t" }, FnType::MODULE, { .modfn = time_diff }, true } );
+	vm.funcs.add( { "-", 2,  2, { "time_point_t", "time_point_t" }, FnType::MODULE, { .modfn = time_diff }, true } );
 }

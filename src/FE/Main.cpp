a6b2ae865389d9cbda4f59415242218220c3015e
Electrux <ElectruxRedsworth@gmail.com>
2019-09-08 10:44:49 +0530

    minor correction

--- src/FE/Main.cpp
+++ src/FE/Main.cpp
@@ -56,8 +56,9 @@ int main( int argc, char ** argv )
 	main_src->name = main_src_str;
 	main_src->dir = src_dir;
 	err = tokenize( * main_src );
-	main_src->name = "main_src_" + main_src->name;
 	if( err != E_OK ) goto cleanup;
+
+	main_src->name = "main_src_" + main_src->name;
 
 	if( flags & OPT_T ) {
 		fprintf( stdout, "Tokens:\n" );
